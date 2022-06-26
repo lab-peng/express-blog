fc43f20425cfc235c03b013412ae01a7bc9094a5
Robert <panerobert@yahoo.com>
2022-06-26 19:17:34 +0800

    ok

--- app.js
+++ app.js
@@ -3,19 +3,17 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
 const blogApiRoutes = require('./routes/blogApiRoutes')
-const path = require ('path');
 
 const app = express();
 app.set('view engine', 'ejs')
-app.listen(3000)
 
 // const dbURL = 'mongodb://localhost:27017'
-// const dbURL = 'mongodb+srv://root:hello123@cluster0.l0iylyh.mongodb.net/test?retryWrites=true&w=majority'
+const dbURL = 'mongodb+srv://root:hello123@cluster0.l0iylyh.mongodb.net/test?retryWrites=true&w=majority'
 // const dbURL = process.env.dbURL;
 // const dbURL = process.env.MONGODB_URI;
-// mongoose.connect(dbURL)
-//     .then(() => app.listen(3000))
-//     .catch(err => console.log(err))
+mongoose.connect(dbURL)
+    .then(() => app.listen(8000))
+    .catch(err => console.log(err))
 
 // middleware
 // app.use((req, res, next) => {
@@ -26,10 +24,7 @@ app.listen(3000)
 // });
 
 // use imported middleware
-// app.use(express.static('static'))
-app.use(express.static(path.join(__dirname + '/static')))
-app.set('views', path.join(__dirname, '/views'));
-
+app.use(express.static('static'))
 app.use(express.urlencoded({ extended: true }));  // middleware for submit form data
 app.use(morgan('dev'));
 
--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -1,22 +1,13 @@
 const Blog = require('../models/blog');
 
-
-const blogs = [
-    { id: '1', title: 'How Many Roads Must A Man Walks Before You Can Call Him A Man?', snippet: 'Each node in a singly-linked list contains not only the value but also a reference field to link to the next node. By this way, the singly-linked list organizes all the nodes in a sequence.', body: 'Each node in a singly-linked list contains not only the value but also a reference field to link to the next node. By this way, the singly-linked list organizes all the nodes in a sequence. Singly Linked List', snippet: 'Each node in a singly-linked list contains not only the value but also a reference field to link to the next node. By this way, the singly-linked list organizes all the nodes in a sequence.' },
-    { id: '2', title: 'Reading and writing JSON files in Node.js', snippet: 'JavaScript Object Notation, referred to as JSON in short, is one of the most popular formats for data storage and data interchange over the internet. The simplicity of the JSON syntax makes it very easy for humans and machines to read and write.', body: 'JavaScript Object Notation, referred to as JSON in short, is one of the most popular formats for data storage and data interchange over the internet. The simplicity of the JSON syntax makes it very easy for humans and machines to read and write. JavaScript Object Notation, referred to as JSON in short, is one of the most popular formats for data storage and data interchange over the internet. The simplicity of the JSON syntax makes it very easy for humans and machines to read and write.'},
-    { id: '3', title: 'Optimize an Express Server in Node.js', snippet: 'We can optimize some aspects of our Express API server to increase the performance of a Node.js application. Two major strategies are caching requests and compressing responses. Compression reduces the size of responses that are sent back to clients, saving bandwidth both for the server and for the client. Caching allows you to reuse retrieved data to speed up serving requests to your API.', body: 'We can optimize some aspects of our Express API server to increase the performance of a Node.js application. Two major strategies are caching requests and compressing responses. Compression reduces the size of responses that are sent back to clients, saving bandwidth both for the server and for the client. Caching allows you to reuse retrieved data to speed up serving requests to your API. We can optimize some aspects of our Express API server to increase the performance of a Node.js application. Two major strategies are caching requests and compressing responses. Compression reduces the size of responses that are sent back to clients, saving bandwidth both for the server and for the client. Caching allows you to reuse retrieved data to speed up serving requests to your API.' },
-    { id: '4', title: 'Whatever Will Be Will Be', snippet: 'We can optimize some aspects of our Express API server to increase the performance of a Node.js application. Two major strategies are caching requests and compressing responses. Compression reduces the size of responses that are sent back to clients, saving bandwidth both for the server and for the client. Caching allows you to reuse retrieved data to speed up serving requests to your API.', body: 'We can optimize some aspects of our Express API server to increase the performance of a Node.js application. Two major strategies are caching requests and compressing responses. Compression reduces the size of responses that are sent back to clients, saving bandwidth both for the server and for the client. Caching allows you to reuse retrieved data to speed up serving requests to your API. We can optimize some aspects of our Express API server to increase the performance of a Node.js application. Two major strategies are caching requests and compressing responses. Compression reduces the size of responses that are sent back to clients, saving bandwidth both for the server and for the client. Caching allows you to reuse retrieved data to speed up serving requests to your API.'}
-]
-
 const blog_list = (req, res) => {
-    // Blog.find().sort({'createdAt': -1})
-    //     .then(result => {
-    //         res.render('blog_views/blog_list', { title: 'Home', blogs: result})
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
-    res.render('blog_views/blog_list', { title: 'Home', blogs: blogs})
+    Blog.find().sort({'createdAt': -1})
+        .then(result => {
+            res.render('blog_views/blog_list', { title: 'Home', blogs: result})
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
 const blog_create_get = (req, res) => {
@@ -25,30 +16,24 @@ const blog_create_get = (req, res) => {
 
 const blog_create_post =  (req, res) => {
     // console.log(req.body);
-    // const blog = new Blog(req.body);
-    // blog.save()
-    //     .then(() => {
-    //         res.redirect('/blogs')
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
-    
-    res.redirect('/blogs')
+    const blog = new Blog(req.body);
+    blog.save()
+        .then(() => {
+            res.redirect('/blogs')
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
 const blog_detail = (req, res) => {
-    // Blog.findById(req.params.id)
-    //     .then(result => {
-    //         res.render('blog_views/blog_detail', { title: result.title, blog: result})
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
-    console.log(req.params.id)
-    const blog = blogs.filter((blog) => blog.id === req.params.id)[0]
-    console.log(blog.id)
-    res.render('blog_views/blog_detail', { title: blog.title, blog: blog })
+    Blog.findById(req.params.id)
+        .then(result => {
+            res.render('blog_views/blog_detail', { title: result.title, blog: result})
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
 const blog_delete = (req, res) => {
